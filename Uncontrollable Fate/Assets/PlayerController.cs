9280b5154b812d53ef71b7da12b410fe87804ce5
fishbigger <toby.e.m.johnson@gmail.com>
2021-01-15 20:17:37 +0000

    Started smoothing movement
    There is still some work to do but I have started smoothing out the movement to make it more intuitive.

--- Uncontrollable Fate/Assets/PlayerController.cs
+++ Uncontrollable Fate/Assets/PlayerController.cs
@@ -4,30 +4,79 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float thrust;
+    public Rigidbody2D rigidBody;
+
+    public Vector2 startPos;
+
+    public float movementSpeed = 10.0f;
+    public float jumpSpeed = 5.0f;
+
+    private bool isGrounded = false;
+
+    private int sideMultiplier;
+    private int jumpMultiplier;
 
     public float chance = 0.1f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)){
-            rb.AddForce(Vector3.up * thrust * getMultiplier());
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            sideMultiplier = getMultiplier();
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
-            rb.AddForce(Vector3.left * thrust * getMultiplier());
+            sideMultiplier = getMultiplier();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpMultiplier = getMultiplier();
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
-            rb.AddForce(Vector3.right * thrust * getMultiplier());
+            Vector2 currentVelocity = rigidBody.velocity;
+            rigidBody.velocity = new Vector2(movementSpeed * sideMultiplier, currentVelocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            Vector2 currentVelocity = rigidBody.velocity;
+            rigidBody.velocity = new Vector2(-movementSpeed * getMultiplier(), currentVelocity.y);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                Vector2 currentVelocity = rigidBody.velocity;
+                rigidBody.velocity = new Vector2(currentVelocity.x, jumpSpeed * jumpMultiplier);
+            }
+        }
+
+        if (transform.position.y < -5)
+        {
+            transform.position = startPos;
         }
     }
 
     int getMultiplier()
     {
         if (Random.Range(0.0f, 1.0f) > chance) { return 1; } else { return -1; }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            isGrounded = false;
+        }
     }
 }
