37cadb0e54ba7154743a36bca010a733ec6035b8
fishbigger <toby.e.m.johnson@gmail.com>
2021-01-16 18:12:14 +0000

    Added level2 and a bunch of other stuff

---
+++ Uncontrollable Fate/Assets/Scripts/elevatorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class elevatorController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    public SpriteRenderer spriteRenderer;
+
+    public int currentSceneNum;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.tag == "elevator")
+        //{
+        //    spriteRenderer.sortingOrder = 4;
+        //    animator.SetTrigger("Close");
+        //}
+        if (collision.gameObject.tag == "elevator")
+        {
+            SceneManager.LoadScene(currentSceneNum + 1);
+        }
+            
+    }
+}
